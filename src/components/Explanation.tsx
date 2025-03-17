bd02f160522800ca5ba9a339a12b1de3336c752b
Nicholas Zhang <nzhan01@bu.edu>
2025-03-17 15:07:54 -0400

    added another picture and text

--- src/components/Explanation.tsx
+++ src/components/Explanation.tsx
@@ -29,21 +29,35 @@ const StyledText = styled.div`
 export default function Explanation() {
 
     return(
+        <>
+            <StyledContainer>
+                <StyledImage src = "anything_album.png" alt = "album cover of adrienne Lenker's anything album"/>
+                <StyledText>
+                    <p>
+                        No one will ever know you the way you know yourself. it's impossible to translate your entire experience into words or pictures. you could spend a lifetime speaking, writing your emotions,
+                        but no matter how carefully you choose your words, they’ll always fall short.
+                        Even when someone listens, with all their heart, they could only grasp a fraction of what it truly means to be you. Language is limited, but human experience is infinite.
+                        Every memory you’ve ever had, every feeling, those are all yours.
+                    </p>
+                </StyledText>
+
+            </StyledContainer>
+        <br/>
+            <StyledContainer>
+                <StyledText>
+                    <p>
+                        Memory is incomplete. There are a million different moments that you simply will not remember.
+                        Even experiences you once thought life-changing may become whispers of memory as you grow older.
+                        But the memories that we actively <i>choose</i> to hold onto are the ones that shape us.
+                    </p>
+                </StyledText>
+                <StyledImage src = "planet.png" alt = "colorful planet"/>
+
+            </StyledContainer>
+        </>
 
 
 
-        <StyledContainer>
-            <StyledImage src = "anything_album.png" alt = "album cover of adrienne Lenker's anything album"/>
-            <StyledText>
-                <p>
-                    No one will ever know you the way you know yourself. it's impossible to translate your entire experience into words or pictures. you could spend a lifetime speaking, writing your emotions,
-                    but no matter how carefully you choose your words, they’ll always fall short.
-                    Even when someone listens, with all their heart, they could only grasp a fraction of what it truly means to be you. Language is limited, but human experience is infinite.
-                    Every memory you’ve ever had, every feeling, those are all yours.
-                </p>
-            </StyledText>
-
-        </StyledContainer>
     )
 
 
